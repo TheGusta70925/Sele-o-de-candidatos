d9ebac42e612f9ee828a6a9d47b7e56c62f565d6
TheGusta <lg70925@gmail.com>
2024-08-15 18:18:36 -0300

    testando

--- Candidatos/src/App.java
+++ Candidatos/src/App.java
@@ -1,5 +1,17 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        analisarCandidatos(1900.0);
+        analisarCandidatos(2000.0);
+        analisarCandidatos(2200.0);
+    }
+    static void analisarCandidatos(double salarioPretendido) {
+        double salarioBase = 2000.0;
+        if (salarioPretendido > salarioBase) {
+            System.out.println("Aguardando resultado dos demais candidatos.");
+        } else if (salarioPretendido == salarioBase) {
+            System.out.println("Ligar para o candidato com contra proprosta.");
+        } else {
+            System.out.println("Ligar para candidato.");
+        }
     }
 }
